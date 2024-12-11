50fbfa204f0b17573ad1d69e24ae310497e242c0
mie-intel <paryapradhanika@gmail.com>
2024-12-11 22:42:38 +0700

    feat[Initialize Helper]

---
+++ app/helper/FilmProvider.cs
@@ -0,0 +1,30 @@
+namespace App.Helper
+{
+    public class FilmProvider
+    {
+        public static string book(string userId, string seat, string idFilm)
+        {
+            string username = "";
+            return username;
+        }
+
+        public static string create(
+            string title,
+            string date,
+            int price,
+            int rate,
+            string description,
+            string username,
+            string password
+        )
+        {
+            string user = "";
+            return user;
+        }
+
+        public static string delete(string id)
+        {
+            return "A";
+        }
+    }
+}
---
+++ app/helper/UserProvider.cs
@@ -0,0 +1,25 @@
+namespace App.Helper
+{
+    public class UserProvider
+    {
+        public static string handleLogin(string username, string password)
+        {
+            return username;
+        }
+
+        public static string handleRegister(string username, string password)
+        {
+            return username;
+        }
+
+        private static int getCurrentSaldo()
+        {
+            return 0;
+        }
+
+        public static string handleTopUp(string username, string password, int money)
+        {
+            return "A";
+        }
+    }
+}
