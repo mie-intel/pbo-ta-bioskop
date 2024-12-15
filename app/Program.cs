20316fcce94a050b70cec393881c21d1e1254f3a
mie-intel <paryapradhanika@gmail.com>
2024-12-15 12:24:19 +0700

    fix[FilmDb]: mengembalikan ke program semula

--- app/Program.cs
+++ app/Program.cs
@@ -10,23 +10,23 @@ namespace App
         // Program Utama
         static void Main(string[] args)
         {
-            // while (true)
+            // // string tes = UserProvider.Register("polikarpu123", "mie-mie");
+            // // Console.WriteLine(tes);
+            // // Console.WriteLine(FilmProvider.GenerateSeatCode(3));
+            // FilmProvider.AddFilm("Ipar adalah maut", "23/12/2024", 5, 1000, 10, "Aku makan teri");
+            // FilmProvider.AddFilm("Ipar2", "23/12/2024", 5, 1000, 10, "Aku makan teri");
+            // FilmProvider.AddFilm("Ipar3", "23/12/2024", 5, 1000, 10, "Aku makan teri");
+            // FilmProvider.AddFilm("Ipar4", "23/12/2024", 5, 1000, 10, "Aku makan teri");
+            // var Seat = FilmProvider.ParseSeat("FPeT3G1");
+            // // Console.WriteLine(Seat);
+            // foreach (var p in Seat)
             // {
-            //     if (!TerminalDisplay.Display())
-            //         break;
+            //     Console.WriteLine(p.Item1 + " " + p.Item2);
             // }
-            // string tes = UserProvider.Register("polikarpu123", "mie-mie");
-            // Console.WriteLine(tes);
-            // Console.WriteLine(FilmProvider.GenerateSeatCode(3));
-            FilmProvider.AddFilm("Ipar adalah maut", "23/12/2024", 5, 1000, 10, "Aku makan teri");
-            FilmProvider.AddFilm("Ipar2", "23/12/2024", 5, 1000, 10, "Aku makan teri");
-            FilmProvider.AddFilm("Ipar3", "23/12/2024", 5, 1000, 10, "Aku makan teri");
-            FilmProvider.AddFilm("Ipar4", "23/12/2024", 5, 1000, 10, "Aku makan teri");
-            var Seat = FilmProvider.ParseSeat("FPeT3G1");
-            // Console.WriteLine(Seat);
-            foreach (var p in Seat)
+            while (true)
             {
-                Console.WriteLine(p.Item1 + " " + p.Item2);
+                if (!TerminalDisplay.Display())
+                    break;
             }
             Console.WriteLine("\nSampai Jumpa!");
         }
